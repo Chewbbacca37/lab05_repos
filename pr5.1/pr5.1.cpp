19018a77c418cf0cc5c620d7088ec5435a72d5bf
Chewbbacca37 <mr.kolyasiks20150@gmail.com>
2022-11-03 22:09:27 +0200

    first

---
+++ pr5.1/pr5.1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double k(double const x, double const y);
+
+int main() {
+	double p, q;
+
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+
+	double res = (k(1 + p * q, pow(q, 2)) + pow(k(p, pow(p, 2)), 2))
+		/ (1 + k(p * q + pow(q, 2), p));
+
+	cout << "res = " << res << endl;
+
+	return 0;
+}
+
+double k(double const x, double const y)
+{
+	return (x / (1 + pow(sin(y), 2) + y / (1 + pow(x, 2))));
+}
